a01f9306d4f2241c7d1731d39840f3b4f35bbbbb
lets00 <eduardovansilva@gmail.com>
2023-08-17 22:16:35 -0300

    Adicionando color: black aos parágrafos e títulos

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,22 +1,26 @@
 import Link from 'next/link'
-import Image from  'next/image'
+import Image from 'next/image'
 import styles from '../styles/Layout.module.css'
 
-export default function Layout(props: any){
+export default function Layout(props: any) {
     return (
-        <div className={styles.Layout}>
-            <div className={styles.cabecalho}>
-                <Image src='/images/FELIPEALVESRBG2.png' width='250' height='100' alt='imagemDoCurso'/>
-                <Link href="/">Cursos</Link>
-                <Link href="quemSomos">Quem somos</Link>
-                <Link href="/correcao">Correção de redação</Link>
+        <div className={`bg-white flex flex-col flex-wrap m-6 rounded-lg text-black`}>
+            <header className={`flex m-6 justify-between items-center`}>
+                <div>
+                    <Image src='/images/FELIPEALVESRBG2.png' width='250' height='100' alt='imagemDoCurso' />
+                </div>
+                <div className={`flex justify-between items-center`}>
+                    <Link className={`mr-2`} href="/">Cursos</Link>
+                    <Link className={`mr-2`} href="quemSomos">Quem somos</Link>
+                    <Link className={`mr-2`} href="/correcao">Correção de redação</Link>
+                </div>
                 <Link href="/login" className={styles.entrar}>Entrar</Link>
                 <Link href="/cadastro" className={styles.cadastro}>Cadastrar</Link>
-            </div>
+            </header>
             <div className={styles.conteudo}>
                 {props.children}
             </div>
-            <div style={{backgroundColor: '#373E48', color: 'white'}}>
+            <div style={{ backgroundColor: '#373E48', color: 'white' }}>
                 <div className="p-14 px-20">
                     <h1 className="font-Montserrant text-2xl">Curso Felipe Alves</h1>
                     <h4>emailexemplo@gmail.com</h4>
